ea83f79facef08340318da9f10d733f9547f40e4
annamirza28 <annamirza28@gmail.com>
2024-08-26 13:38:36 -0500

    initial files

---
+++ myjavaproject1/src/main/java/com/itexpert/homework/myjavaproject1/Calculator.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.itexpert.homework.myjavaproject1;
+
+/**
+ *
+ * @author Ana
+ */
+public class Calculator {
+  public int add (int x, int y){
+  return x + y;
+  }
+  public int mul (int x, int y){
+  return x * y;
+  }
+}
